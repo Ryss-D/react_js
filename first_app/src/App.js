3fecc21430e70824c6e20e3395724ce2873782b1
Sebastian Yepes Sanchez <70545185+Ryss-D@users.noreply.github.com>
2023-09-16 20:16:46 -0500

    feat: added basic projects to start work around

---
+++ first_app/src/App.js
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    <body>
+      <div>
+        <h2>Let's get started!</h2>
+      </div>
+    </body>
+    </div>
+  );
+}
+
+export default App;
